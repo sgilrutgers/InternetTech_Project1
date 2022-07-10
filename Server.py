9210f85ae78d408f0a6ae71aabceb52ec36d910c
sgilrutgers <109001249+sgilrutgers@users.noreply.github.com>
2022-07-09 20:32:17 -0400

    Update Server.py
    Add-on Server now does not have any problems, what it needs is to change line 59 to answer and fully establish connection to google DNS.

--- Server.py
+++ Server.py
@@ -2,35 +2,60 @@ import argparse
 from sys import argv
 import socket
 
-
-parser=argparse.ArgumentParser(description="""This is a very basic client program""")
-parser.add_argument('port', type=int, help='This is the port to connect to the server on',action='store')
-args = parser.parse_args(argv[1:])
-
-HOST = ''
-PORT = args.port
-
-ss = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-ss.bind((HOST,PORT))
-ss.listen(1)
-conn,address = ss.accept()
-with conn:
-        while True:
-            data = conn.recv(512)
-            if not data:
-                break
-            data = data[::-1]
-            conn.sendall(data)
-
+#for some reason, the code is throwing a bad pipe error if we have the method below the connection to the client
+#
 def getIpList(data):
+    #if you uncomment the return below and the print answer, it will show you that it gets returned four times
+    #return ("made it")
     client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_addr = ("www.google.com", 8.8.8.8)
+    #this is from the given materials reading from routley.io as the DNS server is at 53
+    server_addr("8.8.8.8", 53)
+    #server_addr = ("www.google.com", "8.8.8.8")
+
+    #current issue now is that the server_addr is not wanting to connect, maybe switch to sock_Dgram or treat it as client
+
+
     client_sock.connect(server_addr)
+    
+
     while True:
+        
         client_sock.sendall(data)
         answer = client_sock.recv(1024)
         if not answer:
             break
         list += answer + ","
-  
+
+
+
+    client_sock.close()
     return list
+
+
+
+
+parser = argparse.ArgumentParser(
+    description="""This is a very basic client program""")
+parser.add_argument(
+    'port', type=int, help='This is the port to connect to the server on', action='store')
+args = parser.parse_args(argv[1:])
+
+HOST = ''
+PORT = args.port
+
+ss = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ss.bind((HOST, PORT))
+ss.listen(1)
+conn, address = ss.accept()
+with conn:
+    while True:
+        data = conn.recv(512)
+        if not data:
+            break
+        #There is a bad pipefault occuring here
+        answer = getIpList(data)
+        #print(answer)
+
+        conn.sendall(data)
+
+
